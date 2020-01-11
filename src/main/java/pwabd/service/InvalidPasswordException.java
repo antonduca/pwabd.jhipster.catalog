a30d6d91a967987eea2121909f25571b5c1aad4a
Anton Duca (23242) <anton.duca@upb.ro>
2020-01-11 14:26:58 +0200

    Initial version of catalog generated by JHipster-6.6.0

---
+++ src/main/java/pwabd/service/InvalidPasswordException.java
@@ -0,0 +1,9 @@
+package pwabd.service;
+
+public class InvalidPasswordException extends RuntimeException {
+
+    public InvalidPasswordException() {
+        super("Incorrect password");
+    }
+
+}
---
+++ src/main/java/pwabd/service/UsernameAlreadyUsedException.java
@@ -0,0 +1,9 @@
+package pwabd.service;
+
+public class UsernameAlreadyUsedException extends RuntimeException {
+
+    public UsernameAlreadyUsedException() {
+        super("Login name already used!");
+    }
+
+}
